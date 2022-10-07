6327df2ec833f8a900c71aff9dc32f6fef0cc743
task3 <miraajdarova2@gmail.com>
2022-10-07 17:25:47 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,120 @@
+import com.sun.jdi.PathSearchingVirtualMachine;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /** Бешинчи тема: " Сканнер " */
+
+        Scanner sc = new Scanner(System.in);
+
+      /*  Scanner sc = new Scanner(System.in);
+        int myAge = sc.nextInt();
+        int number = sc.nextInt();
+        System.out.println("Бул менин жашым - " + myAge);
+        System.out.println("Бул менин жашым эмес , бул жон гана сан  - " + number);
+    }
+} */
+
+     /*   Scanner sc = new Scanner(System.in);
+        int myAge = sc.nextInt();
+        int year = 2022;
+        System.out.println(year - myAge);
+    }
+} */
+
+     /*   int number = 10;
+        System.out.println(number);
+        float number2 = (float) (number - 0.2);
+        System.out.println(number2);
+    }
+}*/
+
+      /*  int san1 = 5;
+        int san2 = sc.nextInt();
+        System.out.println(san1 + san2);
+        System.out.println();
+        System.out.println(san1 - san2);
+        System.out.println();
+        System.out.println(san1 * san2);
+        System.out.println();
+        System.out.println(san1 / san2);
+        System.out.println();
+        System.out.println(san1 % san2);
+        System.out.println();
+    }
+}*/
+
+
+      /*  int a3 = sc.nextInt();
+        int b4 = sc.nextInt();
+        int c5 = sc.nextInt();
+        System.out.println(a3 + b4 + c5);
+        System.out.println();
+        System.out.println(a3 * b4 * c5);
+        System.out.println();
+        System.out.println((a3 + b4 + c5) / 3d);
+    }
+} */
+
+     /*  int san1 = sc.nextInt();
+        int san2 = sc.nextInt();
+        int san3 = sc.nextInt();
+        int summa = san1 + san2 + san3;
+        int kob = san1 * san2 * san3;
+        double arif = (san1 + san2 + san3) / 3d;
+        System.out.printf("%d+%d+%d=%d", san1, san2, san3, summa);
+        System.out.println();
+        System.out.printf("%d*%d*%d=%d", san1, san2, san3, kob);
+        System.out.println();
+        System.out.printf("(%d*%d*%d)/3=%f", san1, san2, san3, arif);
+    }
+}*/
+
+      /*  int a3 = sc.nextInt();
+        int b4 = sc.nextInt();
+        int c5 = sc.nextInt();
+        System.out.println(a3 + b4 + c5);
+        System.out.println();
+        System.out.println(a3 * b4 * c5);
+        System.out.println();
+        System.out.println((a3 + b4 + c5) / 3d);
+    }
+}*/
+
+
+        // String str1 = new String(   "Hello  ");
+        // String str2 = "World";
+        // String str3 = str2.substring();
+
+        // System.out.println(str3);
+
+        Scanner scanner = new Scanner(System.in);
+
+        String soz = "Java";
+
+        String soz1 = sc.nextLine();
+
+        System.out.printf("I teach a Language %s", soz1);
+
+        System.out.println();
+
+        System.out.println(soz1 + 4);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
